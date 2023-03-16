846dd7676bb550d2c1fe2758bd2f0111481c1d4f
kayode <officialkayodeolowo@gmail.com>
2023-03-17 00:43:16 +0100

    nav bar fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,55 +13,59 @@ export default function Navbar({ fixed }) {
 
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-500 mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-              href="/"
-            >
-              Logo
-            </a>
-            <button
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-            {navbarOpen ?  <AiOutlineClose className="transition ease-in duration-500" /> :  < AiOutlineMenu className="transition ease-in duration-500"/>}
-            </button>
-          </div>
-          <div
-            className={
-              "lg:flex flex-grow items-center" +
-              (navbarOpen ? " flex" : " hidden")
-            }
-            id="example-navbar-danger"
+     <nav className="fixed w-full top-0 z-50 flex flex-wrap items-center justify-between px-2 py-3 bg-pink-500 mb-3">
+  <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+      <a
+        className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+        href="/"
+      >
+        Logo
+      </a>
+      <button
+        className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+        type="button"
+        onClick={() => setNavbarOpen(!navbarOpen)}
+      >
+        {navbarOpen ? (
+          <AiOutlineClose className="transition ease-in duration-500" />
+        ) : (
+          <AiOutlineMenu className="transition ease-in duration-500" />
+        )}
+      </button>
+    </div>
+    <div
+      className={
+        "lg:flex flex-grow items-center" + (navbarOpen ? " flex" : " hidden")
+      }
+      id="example-navbar-danger"
+    >
+      <ul
+        onClick={handleNav}
+        className="flex flex-col lg:flex-row list-none lg:ml-auto"
+      >
+        <li className="nav-item">
+          <Link
+            className=" py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+            href="/about"
           >
-            <ul onClick={handleNav}  className="flex flex-col lg:flex-row list-none lg:ml-auto">
-              <li className="nav-item">
-                <Link
-                  className=" py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="/about"
-                  
-                >
-                  About
-                </Link>
-              </li>
+            About
+          </Link>
+        </li>
 
-              <li className="nav-item">
-                <Link
-                  className=" py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="/service"
-                  
-                >
-                  service
-                </Link>
-              </li>
-             
-            </ul>
-          </div>
-        </div>
-      </nav>
+        <li className="nav-item">
+          <Link
+            className=" py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+            href="/service"
+          >
+            Service
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
     </>
   );
 }
