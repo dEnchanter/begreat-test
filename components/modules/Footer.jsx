5f7e16d87df24ed08c758c6736969d73721b71a2
kayodeOlowo <officialkayodeolowo@gmail.com>
2023-03-27 07:09:26 +0100

    homepage

--- components/modules/Footer.jsx
+++ components/modules/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
            {/* <img className='h-[17rem]  absolute mt-[-2rem] left-[30%] lg:left-[35%] 2xl:left-[40%] hidden xl:flex' src='./images/homepage/shape.png' alt=' ' /> */}
         <div className=' lg:flex justify-between pb-10 lg:px-10'>
             <div className='lg:w-[15rem] w-fit mx-auto lg:mx-0  lg:mt-0'> 
-                <img className='h-10 w-fit mx-auto lg:mx-0'  src={theme=="light"?"/images/homepage/logoD.png":"/images/homepage/logoW.png" } alt='logo'/>
+                <img className='h-10  mx-auto lg:mx-0'  src={theme=="light"?"/images/homepage/logoD.png":"/images/homepage/logoW.png" } alt='logo'/>
                 <p className='lg:text-[0.79rem] text-xs mt-2 text-center lg:text-start sm:w-[70%]  sm:mx-auto lg:w-full lg:mx-0 workstext leading-4 '> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa aut explicabo natus aliquid nostrum! Totam harum aspernatur </p>
                 <div className='flex space-x-4 mt-4 w-fit mx-auto lg:mx-0 text-transparent bg-clip-text bg-gradient-to-r from-[#BE3677] to-[#A64AA2]'>  
                     <BsFacebook className='hover:cursor-pointer text-transparent bg-clip-text bg-gradient-to-r from-[#BE3677] to-[#A64AA2] '/>
--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -13,8 +13,8 @@ export default function pricing() {
           <p className='text-center text-sm'> Our pricing plans are designed to be affordable, flexible, and scalable, so you can <br className='hidden md:flex'/> choose the plan that best fits your investment goals. </p>
         </div>
 
-       <div className='flex md:flex-row flex-col justify-center space-y-4 md:space-y-0 md:space-x-4 mt-10 pb-10'> 
-           <div className=' rounded-md shadow-lg  hover:shadow-2xl hover:cursor-pointer h-[23rem]  w-full  md:mx-0 mx-auto sm:w-[18rem] px-6 priceback'> 
+       <div className='flex md:flex-row flex-col justify-center space-y-4 md:space-y-0 md:space-x-6 mt-10 pb-10'> 
+           <div className=' rounded-md shadow-lg  lg:hover:shadow-2xl hover:cursor-pointer h-[23rem]  w-full  md:mx-0 mx-auto max-w-[19rem] sm:max-w-[18rem] px-6 priceback'> 
           <h1 className='font-bold text-xl pt-4'>Monthly Plan </h1>
           <p className='text-xs mt-2'> All the basics for starting a new business </p>
           <h1 className='mt-4 text-xs'> <span className='text-3xl font-bold'>$50</span>/MO </h1>
@@ -27,7 +27,7 @@ export default function pricing() {
           </div>
         </div>
 
-        <div className=' rounded-md shadow-lg  hover:shadow-2xl hover:cursor-pointer h-[23rem]  w-full md:mx-0 mx-auto sm:w-[18rem] px-6 priceback'> 
+        <div className=' rounded-md shadow-lg  lg:hover:shadow-2xl hover:cursor-pointer h-[23rem]  w-full md:mx-0 mx-auto max-w-[19rem] sm:max-w-[18rem] px-6 priceback'> 
           <h1 className='font-bold text-xl pt-4'>Yearly Plan </h1>
           <p className='text-xs mt-2'> All the basics for starting a new business </p>
           <h1 className='mt-4 text-xs'> <span className='text-3xl font-bold'>$500</span>/YR </h1>
