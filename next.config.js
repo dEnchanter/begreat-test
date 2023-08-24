0302e3d491d86d5f5980e4811bb1b5cc753bb79f
dEnchanter <afolabiopaleye@gmail.com>
2023-08-24 10:16:58 +0100

    ..

--- next.config.js
+++ next.config.js
@@ -6,12 +6,10 @@ const nextConfig = {
     domains: ['lh3.googleusercontent.com','firebasestorage.googleapis.com'],
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    if (!dev) {  // Only run for production builds
       config.plugins.push(new webpack.IgnorePlugin({
         // Ignore all pages except dashboard/index.js
         resourceRegExp: /^\.\/pages\/(?!dashboard\/index\.js$)/,
       }));
-    }
 
     return config;
   },
