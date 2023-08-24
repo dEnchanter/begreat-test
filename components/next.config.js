5a48cf9ea0cba6ce08970d5c4b5dec34b1b885db
dEnchanter <afolabiopaleye@gmail.com>
2023-08-24 08:42:15 +0100

    ..

--- components/next.config.js
+++ components/next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  distDir: 'dashboard_out'
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 }
+
+module.exports = nextConfig
