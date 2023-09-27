58fe1169c309a89818aa20055a06e258296a7bc7
dEnchanter <afolabiopaleye@gmail.com>
2023-09-27 13:52:34 +0100

    ..

--- Hoc/ProctectedRoute.js
+++ Hoc/ProctectedRoute.js
@@ -25,7 +25,7 @@ export const ProtectedRoute = ({ children, type }) => {
   const loadingNAhs = useSelector(selectLoading); // Add isLoading from Redux store
 
   const { data, isLoading, error, refetch, isError, status} = useGetUserProfileQuery(); // Use the generated hook
-  console.log("datadata", data)
+   console.log("datadata", data)
   const { data:dataStatus, isLoading:statusLoader, error:errorLoader, isError:statusIsError } = useCheckStatusQuery(); // Use the generated hook
   console.log("data status", dataStatus)
   
